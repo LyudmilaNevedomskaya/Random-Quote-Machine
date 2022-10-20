0c4727aa39d58ae68602321d9abd2c04b58c4723
Lyudmila <milo4kamoya@gmail.com>
2022-10-19 20:18:05 -0700

    box elements and styles added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee, faSquareNfi, faQuoteRight } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee, faSquareNfi, faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 
 function App() {
@@ -10,14 +10,16 @@ function App() {
   })
   return (
     <main className='main-container'>
-      <div id='quote-box' className="App">
-        <p id='text'><FontAwesomeIcon icon={faQuoteRight} /> {quote.content}</p>
+      <div id='quote-box'>
+        <p id='text'><FontAwesomeIcon icon={faQuoteLeft} /> {quote.content}</p>
         <p id='author'>- {quote.author}</p>
-        <div>
-          <a><FontAwesomeIcon icon={faSquareNfi} /></a>
-          <a><FontAwesomeIcon icon={faCoffee} /></a>
+        <div className='social-button-container'>
+          <div className='social-container'>
+            <a id='tweet-quote' href='https://twitter.com/intent/tweet' target="_blank" ><FontAwesomeIcon icon={faSquareNfi} /></a>
+            <a href='https://twitter.com/intent/tweet' target="_blank"><FontAwesomeIcon icon={faCoffee} /></a>
+          </div>
+          <button id='new-quote'>New Quote</button>
         </div>
-        <button id='new-quote'>New Quote</button>
       </div>
       <span>by Mila</span>
     </main>
