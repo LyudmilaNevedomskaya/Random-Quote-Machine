3010f45909415a1c9b13d0979232dccf967a988a
Lyudmila <milo4kamoya@gmail.com>
2022-10-19 21:19:57 -0700

    fetching a quote on button click

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee, faSquareNfi, faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
@@ -8,6 +8,17 @@ function App() {
     content: 'You become what you believe.',
     author: 'Oprah Winfrey'
   })
+
+  const handleClick = async () => {
+    try {
+      const data = await (await fetch(`https://zenquotes.io/api/random`)).json()
+      console.log(data)
+      setQuote({content: data[0].q, author: data[0].a})
+  } catch (err) {
+      console.log('ERROR', err.message)
+  }
+  }
+      
   return (
     <main className='main-container'>
       <div id='quote-box'>
@@ -18,7 +29,7 @@ function App() {
             <a id='tweet-quote' href='https://twitter.com/intent/tweet' target="_blank" ><FontAwesomeIcon icon={faSquareNfi} /></a>
             <a href='https://twitter.com/intent/tweet' target="_blank"><FontAwesomeIcon icon={faCoffee} /></a>
           </div>
-          <button id='new-quote'>New Quote</button>
+          <button id='new-quote' onClick={handleClick}>New Quote</button>
         </div>
       </div>
       <span>by Mila</span>
